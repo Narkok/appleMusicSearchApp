6bfd34a48cc87e1c8725d11e8e865b7c621c423c
Narek Stepanyan <nar-ste@yandex.ru>
2019-09-12 00:21:55 +0300

    Созданы файлы проекта

---
+++ appleMusicSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appleMusicSearchApp
+//
+//  Created by Narek Stepanyan on 12/09/2019.
+//  Copyright © 2019 NRKK dev.studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
